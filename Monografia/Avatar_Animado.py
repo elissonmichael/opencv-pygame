c6f7c3fe396b8a65a9f7a6ab34ccd807955a22e0
Élisson Michael <elissonmichael@yahoo.com.br>
2012-07-01 13:00:10 -0300

    parado vira animacao

--- Monografia/Avatar_Animado.py
+++ Monografia/Avatar_Animado.py
@@ -2,12 +2,11 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode([640,480])
-mainloop, x,y, color, fontsize, delta, fps =  True, 25 , 0, (32,32,32), 35, 1, 30
+fps =30
 clock = pygame.time.Clock()
 fundo_ambiente_virtual = pygame.image.load('animacoes/fundo.jpg').convert()
-parado = pygame.image.load('animacoes/standing.png').convert_alpha()
 
-
+parado = []
 levantar_ambos_os_bracos = []
 levantar_braco_direito = []
 levantar_braco_esquerdo = []
@@ -15,6 +14,7 @@ caminhar_direita = []
 caminhar_esquerda = []
 
 for i in range(0,29):
+    parado.append(pygame.image.load("animacoes/standing/"+str(i) +".png").convert_alpha())
     levantar_ambos_os_bracos.append(pygame.image.load("animacoes/both_arms/"+str(i) +".png").convert_alpha())
     levantar_braco_direito.append(pygame.image.load("animacoes/right_arm/"+str(i) +".png").convert_alpha())
     levantar_braco_esquerdo.append(pygame.image.load("animacoes/left_arm/"+str(i) +".png").convert_alpha())
@@ -25,4 +25,5 @@ frame_da_animacao=0
 estado_avatar = parado
 posicao_X_do_avatar = 100
 posicao_Y_do_avatar = 160
+sequencia_de_animacoes = []
 
